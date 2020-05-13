2969a2831949b3fdb613ccc76c359514eb3cb7b6
GO Lee <52603436+goleedev@users.noreply.github.com>
2020-05-13 20:12:22 +0900

    Initial commit

---
+++ intro-to-node/index.js
@@ -0,0 +1,10 @@
+//jshint esversion:6
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var myVillainName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(myVillainName);
